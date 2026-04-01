4703644fd5f61db2f904db9e8fcf37d12d8f02cc
Hope-Eliot <cf.learningcybersec@gmail.com>
2026-04-01 14:50:18 -0300

    feat: add includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+</main>
+<footer class="footer">
+    <p>CYCLEVIE &mdash; BUT Réseaux &amp; Télécommunications &mdash; IUT Kourou</p>
+</footer>
+<script src="/public/js/main.js"></script>
+</body>
+</html>
